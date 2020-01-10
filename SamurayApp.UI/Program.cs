3bf89fb1273155f5102b3c942fd24c6423c01b18
Julio Negron <jnegron215@gmail.com>
2020-01-10 12:32:54 -0500

    Added methods for retrieving, filtering and modifying data

--- SamurayApp.UI/Program.cs
+++ SamurayApp.UI/Program.cs
@@ -3,6 +3,7 @@ using SamuraiApp.Domain;
 using SamuraiApp.Data;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace SamurayApp.UI
 {
@@ -12,6 +13,7 @@ namespace SamurayApp.UI
 
         static void Main()
         {
+            #region module 4 calls
             //InsertSamurai();
             //InsertMultipleSamrais();
             //InsertMultipleDifferentObjects();
@@ -24,10 +26,184 @@ namespace SamurayApp.UI
             //QueryAndUpdateBattle_Disconnected();
             //AddSomeMoreSamurais();
             //DeleteWhileTracked();
-            DeleteWhileNotTracked();
+            //DeleteWhileNotTracked();
             //DeleteMany();
-            //DeleteUsingId(4);
+            //DeleteUsingId(4); 
+            #endregion
+
+            //InsertNewPkFkGraph();
+            //InsertNewPkFkGraphMultipleChildren();
+            //AddChildToExistingObjectWhileTracked();
+            //AddChildToExistingObjectWhileNotTracked(7);
+            //EagerLoadSamuraiWithQuotes();
+            //ProjectSomeProperties();
+            //ProjectSamuraisWithQuotes();
+            //ProjectSamuraisWithQuotesFiltered();
+            //ProjectSomeProperties();
+            //ChildFilteringWithRelatedData();
+            ModifyingRelatedDataWhenTracked();
         }
+
+        private static void ModifyingRelatedDataWhenTracked()
+        {
+            var samurai = _context.Samurais.Include(s => s.Quotes).FirstOrDefault();
+            samurai.Quotes[0].Text += " Did you hear that?";
+            _context.SaveChanges();
+        }
+
+        private static void ModifyingRelatedDataWhenNotTracked()
+        {
+            var samurai = _context.Samurais.Include(s => s.Quotes).FirstOrDefault();
+            var quote = samurai.Quotes[0];
+            quote.Text += " Did you hear that?";
+            using (var newContext = new SamuraiContext())
+            {
+                //newContext.Quotes.Update(quote);
+                newContext.Entry(quote).State = EntityState.Modified;
+                newContext.SaveChanges();
+            }
+        }
+
+        private static void FilteringWithRelatedData()
+        {
+            var samurais = _context.Samurais
+                .Where(s => s.Quotes.Any(q => q.Text.Contains("happy")))
+                .ToList();
+            samurais.ForEach(x => ShowSamuraiWithQuotes(x));
+        }
+
+        private static void ChildFilteringWithRelatedData()
+        {
+            var samurais = _context.Samurais
+                .Select(x => new 
+                    { 
+                        Samurai = x, 
+                        Quotes = x.Quotes.Where(q => q.Text.Contains("happy")).ToList() 
+                    })
+                .ToList();
+            //show me the results
+            foreach (var item in samurais)
+            {
+                var name = item.Samurai.Name;
+                item.Quotes.ForEach(q => Console.WriteLine($"{name} says: {q.Text}"));
+            }
+        }
+
+        private static void ShowSamuraiWithQuotes(Samurai samurai)
+        {
+            Console.WriteLine(samurai.Name);
+            samurai.Quotes.ForEach(x => Console.WriteLine(x.Text));
+        }
+
+        private static void ProjectSamuraisWithQuotes()
+        {
+            //Project to anonumous type
+            var somePropertiesWithQuotes = _context.Samurais
+                .Select(s => new { s.Id, s.Name, s.Quotes.Count })
+                .ToList();
+            somePropertiesWithQuotes.ForEach(x => Console.WriteLine($"Id: {x.Id}, Name: {x.Name}, Quotes: {x.Count}"));
+        }
+
+        private static void ProjectSamuraisWithQuotesFiltered()
+        {
+            //Project to anonumous type and filter by Count
+            var somePropertiesWithQuotes = _context.Samurais
+                .Select(s => new { s.Id, s.Name, s.Quotes.Count })
+                .Where(x => x.Count > 0)
+                .ToList();
+            somePropertiesWithQuotes.ForEach(x => Console.WriteLine($"Id: {x.Id}, Name: {x.Name}, Quotes: {x.Count}"));
+        }
+
+        public struct IdAndName
+        {
+            public IdAndName(int id, string name)
+            {
+                Id = id;
+                Name = name;
+            }
+            public int Id;
+            public string Name;
+        }
+
+        private static void ProjectSomeProperties()
+        {
+            //anonymous type
+            var someProperties = _context.Samurais.Select(s => new { s.Id, s.Name }).ToList();
+            //specific type
+            var idsAndNames = _context.Samurais.Select(s => new IdAndName(s.Id, s.Name)).ToList();
+            //output
+            someProperties.ForEach(x => Console.WriteLine($"Id: {x.Id}, Name: {x.Name}"));
+            idsAndNames.ForEach(x => Console.WriteLine($"Id: {x.Id}, Name: {x.Name}"));
+        }
+
+        private static List<dynamic> ProjectDynamic()
+        {
+            var someProperties = _context.Samurais.Select(s => new { s.Id, s.Name }).ToList();
+            return someProperties.ToList<dynamic>();
+        }
+
+        private static void EagerLoadSamuraiWithQuotes()
+        {
+            var samuraiWithQuotes = _context.Samurais
+                .Where(s => s.Name.Contains("Kyūzō"))
+                .Include(s => s.Quotes)
+                .Include(s => s.SecretIdentity)
+                .FirstOrDefault();
+        }
+
+        private static void AddChildToExistingObjectWhileNotTracked(int samuraiId)
+        {
+            var quote = new Quote
+            {
+                Text = "Now that I saved you, will you feed me dinner?",
+                SamuraiId = samuraiId
+            };
+            using (var newContext = new SamuraiContext())
+            {
+                newContext.Quotes.Add(quote);
+                newContext.SaveChanges();
+            }
+        }
+
+        private static void AddChildToExistingObjectWhileTracked()
+        {
+            var samurai = _context.Samurais.First();
+            samurai.Quotes.Add(new Quote
+            {
+                Text = "I bet you're happy that I've saved you!"
+            });
+            _context.SaveChanges();
+        }
+
+        private static void InsertNewPkFkGraphMultipleChildren()
+        {
+            var samurai = new Samurai
+            {
+                Name = "Kyūzō",
+                Quotes = new List<Quote> {
+                  new Quote {Text = "Watch out for my sharp sword!"},
+                  new Quote {Text="I told you to watch out for the sharp sword! Oh well!" }
+                }
+            };
+            _context.Samurais.Add(samurai);
+            _context.SaveChanges();
+        }
+
+        private static void InsertNewPkFkGraph()
+        {
+            var samurai = new Samurai
+            {
+                Name = "Kambei Shimada",
+                Quotes = new List<Quote>
+                               {
+                                 new Quote {Text = "I've come to save you"}
+                               }
+            };
+            _context.Samurais.Add(samurai);
+            _context.SaveChanges();
+        }
+
+        #region mdule 4 methods
 
         private static void DeleteUsingId(int samuraiId)
         {
@@ -57,7 +233,7 @@ namespace SamurayApp.UI
             var samurais = _context.Samurais.Where(s => s.Name.Contains("ō"));
             _context.Samurais.RemoveRange(samurais);
             //alternate: _context.RemoveRange(samurais);
-            
+
             //Modified from original to show rows affected
             var rowsAffected = _context.SaveChanges();
             Console.WriteLine($"Rows affected: {rowsAffected}");
@@ -190,5 +366,7 @@ namespace SamurayApp.UI
             //context.Add(new Samurai { Name = "John Doe" });
             context.SaveChanges();
         }
+
+        #endregion
     }
 }
