a1295fc067dc3b22d4e8e209b778c0273a27af11
Julio Negron <jnegron215@gmail.com>
2020-01-14 16:26:27 -0500

    New DBContext to show example of no-tracking setting

---
+++ SamuraiApp.Data/SamuraiContextNoTracking.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Data
+{
+    /// <summary>
+    /// Example for no-tracking/disconnected scenarios.
+    /// </summary>
+    public class SamuraiContextNoTracking : DbContext
+    {
+        public SamuraiContextNoTracking()
+        {
+            //turn off tracking because it does not help in disconnected scenarios.
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+
+
+        public static readonly ILoggerFactory ConsoleLoggerFactory
+          = LoggerFactory.Create(builder =>
+          {
+              builder
+           .AddFilter((category, level) =>
+               category == DbLoggerCategory.Database.Command.Name
+               && level == LogLevel.Information)
+           .AddConsole();
+          });
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+               .UseLoggerFactory(ConsoleLoggerFactory)
+               .EnableSensitiveDataLogging()
+               .UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>().HasKey(s => new { s.SamuraiId, s.BattleId });
+            modelBuilder.Entity<Horse>().ToTable("Horses");
+        }
+
+    }
+}
